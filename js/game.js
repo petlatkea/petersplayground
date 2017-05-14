ca6c2e50c28f22da65fd93d8b8aae70ce6a2247c
petlatkea <petl@kea.dk>
2017-05-14 16:23:00 +0200

    add player object and basic wall-collision

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ var game = {
     q: null
 };
 
+var player = null;
 
 function pageLoaded() {
     createStage();
@@ -60,7 +61,7 @@ function gameLoaded() {
     // prepare sprites
     game.sprites = new createjs.SpriteSheet( {
         "images": [game.q.getResult("sprites")],
-        "frames": {"width":32, "height":32, "regX": 0, "regY":0},
+        "frames": {"width":32, "height":32, "regX": 16, "regY":16},
         "animations": {
             "p_stopped": [0],
             "p_move": [0,2]
@@ -133,16 +134,48 @@ function createStage() {
     createjs.Ticker.on("tick", ticker);
 }
 
-function createPlayer() {
-    game.player = new createjs.Sprite(game.sprites, "p_stopped");
+var Player = {
+    x: 0,
+    y: 0,
+    w: 19,
+    h: 27,
+    speed: 3,
 
+    moveDown() {
+        this.move(0,player.speed);
+    },
+
+    moveUp() {
+        this.move(0,-player.speed);
+    },
+
+    moveLeft() {
+        this.move(-player.speed,0);
+    },
+
+    moveRight() {
+        this.move(player.speed,0);
+    },
+
+    move( xoffset, yoffset ) {
+        if( canMoveTo(this, this.x+xoffset, this.y+yoffset) ) {
+            this.x += xoffset;
+            this.y += yoffset;
+        }
+    }
+}
+
+function createPlayer() {
+    player = new createjs.Sprite(game.sprites, "p_stopped");
+
+    Object.assign(player, Player);
 }
 
 function startGame() {
 
     buildLevel( game.level );
 
-    game.stage.addChild(game.player);
+    game.stage.addChild( player);
 }
 
 
@@ -151,11 +184,17 @@ function buildLevel( level ) {
     console.log("Build level: " + level);
     console.log( game.levels[level] );
 
+    game.map = [];
+
     var map = game.levels[level].map;
 
     for( let y=0; y < map.length; y++) {
+        game.map[y] = [];
         for( let x=0; x < map[y].length; x++ ) {
             var tileId = map[y][x];
+
+            // TODO: modify id for some tiles
+            game.map[y][x] = tileId;
 
             switch(tileId) {
                 case 0:
@@ -164,8 +203,8 @@ function buildLevel( level ) {
                 case 23:    // blue door
                     tileId = "bluedoor";
                     // start player position here:
-                    game.player.x = x*64+32;
-                    game.player.y = y*64+16;
+                    player.x = x*64+32 + player.w/2;
+                    player.y = y*64+16 + player.h/2;
                     break;
                 case 33:
                     tileId = "reddoor";
@@ -186,20 +225,84 @@ function buildLevel( level ) {
     }
 }
 
+function getTileAt( x, y ) {
+    x = Math.floor(x/64);
+    y = Math.floor(y/64);
+
+    return game.map[y][x];
+}
+
+function canWalkOnTile( tile ) {
+    let canwalk = true;
+
+    switch( tile ) {
+            // plain floor
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 14:
+        case 15:
+        case 16:
+        case 17: canwalk = true;
+            break;
+            // doors - only partly walkable (requires relative x and y pos)
+        case 23:
+        case 24:
+        case 25:
+        case 26:
+        case 27:
+
+        case 33:
+        case 34:
+        case 35:
+        case 36:
+        case 37:
+            canwalk = true;
+            break;
+        default:
+            canwalk = false;
+                 }
+
+    return canwalk;
+}
+
+/* return if the given position is valid for the object */
+function canMoveTo( object, xpos, ypos ) {
+    let validposition = true;
+
+    // find the list of tile-coordinates that the object touches (from the given position)
+    // top left
+    let top = ypos-object.h/2;
+    let left = xpos-object.w/2;
+    let bot = ypos+object.h/2;
+    let right = xpos+object.w/2;
+
+    // find tile at x, y
+
+    // TODO: add Relative x and y for tiles that can be partly walked on
+    validposition = canWalkOnTile( getTileAt(left, top) ) && canWalkOnTile( getTileAt(left, bot) ) &&
+        canWalkOnTile( getTileAt(right, top) ) && canWalkOnTile( getTileAt(right, bot) );
+
+    return validposition;
+}
 
 function ticker( event ) {
 
     // move player
     if( keys.left ) {
-        game.player.x--;
+        player.moveLeft();
     } else if( keys.right ) {
-        game.player.x++;
+        player.moveRight();
     }
 
     if( keys.up ) {
-        game.player.y--;
+        player.moveUp();
     } else if( keys.down ) {
-        game.player.y++;
+        player.moveDown();
     }
 
 
